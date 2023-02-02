3b8ea27184de693844d59604577324f0d17a35d7
Mart7anova <mart7anova@gmail.com>
2023-02-02 13:41:17 +0300

    add react-router-dom, routing, lazy routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Counter} from "./components/Counter";
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutAsync} from "./pages/About/About.async";
+import {MainAsync} from "./pages/Main/Main.async";
 
 export const App = () => {
     return (
         <div className='app'>
-            jfjgigui
+            <Link to={'/'}>main</Link>
+            <Link to={'/about'}>about</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainAsync/>}/>
+                    <Route path={'/about'} element={<AboutAsync/>}/>
+                </Routes>
+            </Suspense>
+
             <Counter/>
         </div>
     );

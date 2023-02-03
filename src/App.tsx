c9dddd30fd033f654dbc489f826d8ed2d199e3f1
Mart7anova <mart7anova@gmail.com>
2023-02-03 17:08:07 +0300

    add global theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, {Suspense} from 'react';
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutAsync} from "./pages/About/About.async";
 import {MainAsync} from "./pages/Main/Main.async";
+import {useTheme} from "./theme/useTheme";
+
 
 export const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>toggle theme</button>
+
             <Link to={'/'}>main</Link>
             <Link to={'/about'}>about</Link>
 
